792525e3f84bc67854f6447d4f548bf686156007
katio <onokatio@gmail.com>
2018-04-26 13:18:08 +0900

    update

---
+++ 1/2-blink/LED_blink.c
@@ -0,0 +1,24 @@
+#include "h8-3052-iodef.h"
+#define MILLISEC 14280
+
+
+void sleep(int max){
+	int i,j,k,c;
+
+	for(c=0; c < max;c++)
+		for(i=0; i < MILLISEC ;i++);
+}
+
+int main(void)
+{
+  P9DDR = 0x30;  /* ポート9の初期化(P94--P95を出力に設定) */
+
+
+  while(1)
+  {
+		P9DR = 0x10;  /* 出力データをセット */
+		sleep(10000);
+		P9DR = 0x20;  /* 出力データをセット */
+		sleep(10000);
+  }
+}
